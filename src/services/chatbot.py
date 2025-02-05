96f6f061b8a30205d1ffdbb1fc62db8098f91c53
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2025-02-05 16:58:22 +0100

    .

--- src/services/chatbot.py
+++ src/services/chatbot.py
@@ -5,3 +5,21 @@ from services.input_validate import analyze_message
 from triaje_classification import TriageClassification
 from context_manager import init_context
 
+class ChatbotInit:
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    
+    def initialize_convesation(self, initial_message):
+
+        try:
+            entities = detect_entities(initial_message)
+            context_initial = init_context(initial_message)
+            context = context_initial['context']
+            missing_questions = context_initial['missing_questions']
+
+            symptoms = []
+
+            try:
+                
