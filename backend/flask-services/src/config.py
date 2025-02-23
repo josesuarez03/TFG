430f32b96edcd5f1c2be84d5dadd9ae5a1bc7e7f
josesuarez03 <josysuarez2003@gmail.com>
2025-02-23 15:43:32 +0100

    organización de la estructura del proyecto

--- backend/flask-services/src/config.py
+++ backend/flask-services/src/config.py
@@ -13,12 +13,12 @@ class Config:
     AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
     AWS_REGION = os.getenv("AWS_REGION")
 
-    # Credenciales para la base de datos de postgres
+    '''# Credenciales para la base de datos de postgres
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
-    DB_PORT = int(os.getenv('DB_PORT', 3306))
+    DB_PORT = int(os.getenv('DB_PORT', 3306))'''
 
     # Configuraciones de logging
     LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
