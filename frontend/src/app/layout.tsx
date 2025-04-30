f7f90e922897198a12146024d2b8ee5025007ea9
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2025-04-30 20:32:13 +0200

    ajustar: mejorar la consistencia del formato en layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import ContentLayout from '@/components/layout/ContentLayout';
-import { ThemeProvider } from '@/components/theme-provider';
-import '@/styles/globals.css';
+import React from "react";
+import ContentLayout from "@/components/layout/ContentLayout";
+import { ThemeProvider } from "@/components/theme-provider";
+import "@/styles/globals.css";
 
 export const metadata = {
-  title: {
-    default: 'Medicheck',
-    template: 'Medicheck | %s ',
-  },
-  description: 'Asistente médico y gestión de datos de salud',
+    title: {
+        default: "Medicheck",
+        template: "Medicheck | %s ",
+    },
+    description: "Asistente médico y gestión de datos de salud",
 };
-  
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="es" suppressHydrationWarning>
-      <body className="antialiased">
-        <ThemeProvider>
-          <ContentLayout>{children}</ContentLayout>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="es" suppressHydrationWarning>
+            <body className="antialiased">
+                <ThemeProvider>
+                    <ContentLayout>{children}</ContentLayout>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
