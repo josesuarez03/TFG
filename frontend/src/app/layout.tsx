55830c910144e560a10e8b2daaf7a0c9ff2eeed0
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2025-04-30 20:31:10 +0200

    ajustar: modificar la importación de ThemeProvider y eliminar atributos innecesarios en el layout principal

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { ThemeProvider } from 'next-themes';
 import ContentLayout from '@/components/layout/ContentLayout';
+import { ThemeProvider } from '@/components/theme-provider';
+import '@/styles/globals.css';
 
 export const metadata = {
-    title: {
-      default: 'Medicheck',
-      template: 'Medicheck | %s ',
-    },
-    description: 'Asistente médico y gestión de datos de salud',
-  };
+  title: {
+    default: 'Medicheck',
+    template: 'Medicheck | %s ',
+  },
+  description: 'Asistente médico y gestión de datos de salud',
+};
   
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-
-    return (
-        <html lang="es" className="bg-white dark:bg-gray-900">
-            <body className="antialiased">
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-                    <ContentLayout>{children}</ContentLayout>
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider>
+          <ContentLayout>{children}</ContentLayout>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
