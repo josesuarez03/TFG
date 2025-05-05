1b91aea14662726a6847116923a5a70c58981a44
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2025-05-05 21:24:16 +0200

    ajustar: agregar configuración de reescrituras y orígenes permitidos en next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  allowedDevOrigins: ['*'],
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
