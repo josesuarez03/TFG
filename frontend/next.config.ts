7f54ab6c0c646bdfa931e74987deba2565a3ae19
José Suarez <102729396+josesuarez03@users.noreply.github.com>
2025-05-06 22:53:16 +0200

    ajustar: actualizar la configuración de redirección y encabezados CORS en next.config.ts para mejorar la gestión de API

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  allowedDevOrigins: ['*'],
-
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        destination: 'http://localhost:8000/:path*',
       },
-    ]
+    ];
+  },
+  
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        ],
+      },
+    ];
   },
 };
 
